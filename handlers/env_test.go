558e6ad7e825bb8eed176013c6b5d3b68a93e7fb
Jacek Nikodem <jacek.nikodem@gmail.com>
2016-07-03 22:56:29 +0100

    Environment handler test added.

---
+++ handlers/env_test.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandler(t *testing.T) {
+	k := "TEST_FAKE_ENV_VAR"
+	err := os.Setenv(k, "fake_value")
+	assert.Nil(t, err)
+	defer os.Unsetenv(k)
+
+	h := NewEnvHandler()
+
+	r, _ := http.NewRequest("GET", "/", nil)
+	d := h.Expose(r)
+	info := d.(EnvInfo)
+
+	assert.Contains(t, info.Vars, k)
+}
